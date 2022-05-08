78b3cf856f3813065b95fb6c0c5cbe8f8a5773d9
Madhumidha10 <madhu4achieve@gmail.com>
2022-05-09 00:27:39 +0530

    coomit

--- frontend/src/Components/StudentList.jsx
+++ frontend/src/Components/StudentList.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { NavLink, useNavigate } from "react-router-dom";
 import { API } from "./Api";
 
 export default function StudentList() {
